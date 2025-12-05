6b4d44c121429a76ced9bcc2f84c50d13124aa2a
Lilla Mry <maria.kjellholm@gmail.com>
2025-12-05 10:16:00 +0100

    fixed test setup

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
+    setupFiles: "./src/test/setup.js",
     coverage: {
       provider: "v8",
       threshold: {
